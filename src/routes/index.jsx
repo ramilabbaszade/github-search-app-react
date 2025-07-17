002e2747bdf14e7716e5e8871caee99ea82cfa40
Ramil <56456515+ramilabbaszade@users.noreply.github.com>
2025-07-17 20:01:51 +0200

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,7 +7,6 @@ export const useProjectRoutes = () => {
     const routers = useRoutes(routes)
     return <div className="md:container mx-auto px-28 py-3">
         <Header/>
-        <script src="https://widgifyco.s3.eu-north-1.amazonaws.com/scripts/cmd33exnd0001qablv6tvmf42_1752496914942_bjqqjp.js"></script>
         {routers}
     </div>
 };

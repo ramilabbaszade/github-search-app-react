b99e39501da6f30695d55f3d923e73cbae306a37
Ramil <56456515+ramilabbaszade@users.noreply.github.com>
2025-07-22 19:05:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import {useProjectRoutes} from "./routes/index.jsx";
 
+<script src="https://widgifyco.s3.eu-north-1.amazonaws.com/scripts/cmd33exnd0001qablv6tvmf42_1752496914942_bjqqjp.js"></script>
+
 function App() {
     const projectRoute = useProjectRoutes()
+    <script src="https://widgifyco.s3.eu-north-1.amazonaws.com/scripts/cmd33exnd0001qablv6tvmf42_1752496914942_bjqqjp.js"></script>
     return projectRoute
 }
 
